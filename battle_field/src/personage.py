2ea1d2cf878ef1dd9d463d4c4680dbf5ea3b3dd5
afomin <afomin@topcon.com>
2017-10-16 12:03:45 +0300

    patch for obstacle behaviour

--- battle_field/src/personage.py
+++ battle_field/src/personage.py
@@ -72,88 +72,33 @@ class Personage(QGraphicsPixmapItem):
         # 1. find move_dir as normalized vector between center of
         # item and my center
         my_vect = QVector2D(self.pos())
-        print("my pos = " + str(self.pos()))
         item_vect = QVector2D(item.pos())
-        print("item pos = " + str(item.pos()))
         move_dir = my_vect - item_vect
-        print("move_dir before normalized:")
-        print("move_dir: x = " + str(move_dir.x()))
-        print("move_dir: y = " + str(move_dir.y()))
-
         move_dir = move_dir.normalized()
-
-        print("move_dir: x = " + str(move_dir.x()))
-        print("move_dir: y = " + str(move_dir.y()))
-
         # 2. add projection of all dots to move_dir
         # (vector A * vector B)
-        my_dots_info = self.get_direction_info(self, move_dir)
-        for dot in my_dots_info:
-            print("my dots info: x = " + str(dot[0].x()))
-            print("my dots info: y = " + str(dot[0].y()))
-            print("my dotProduct: " + str(dot[1]))
-
-        item_dots_info = self.get_direction_info(item, move_dir)
-        for dot in item_dots_info:
-            print("item dots info: x = " + str(dot[0].x()))
-            print("item dots info: y = " + str(dot[0].y()))
-            print("item dotProduct: " + str(dot[1]))
-
+        my_dots_info = self.get_direction_info(self, move_dir, item.pos())
+        item_dots_info = self.get_direction_info(item, move_dir, item.pos())
         # 3. sort lists of tuples by projections
         # to move_dir vector
         my_dots_info = sorted(
             my_dots_info,
-            key=lambda item: item[1])
-
-        print("my dots info after sorted")
-        for dot in my_dots_info:
-            print("my dots: x = " + str(dot[0].x()))
-            print("my dots: y = " + str(dot[0].y()))
-            print("my dotProduct: " + str(dot[1]))
+            key=lambda item: item)
 
         item_dots_info = sorted(
             item_dots_info,
-            key=lambda item: item[1])
-
-        print("item dots info after sorted")
-        for dot in item_dots_info:
-            print("item dots: x = " + str(dot[0].x()))
-            print("item dots: y = " + str(dot[0].y()))
-            print("item dotProduct: " + str(dot[1]))
+            key=lambda item: item)
 
         # 4. find distance of moving
-        print(
-            "my_dots_info[len(my_dots_info) - 1][1] = " +
-            str(my_dots_info[len(my_dots_info) - 1][1]))
-        print(
-            "item_dots_info[0][1] = " +
-            str(item_dots_info[0][1]))
-        print(
-            "item_dots_info[len(item_dots_info) - 1][1] = " +
-            str(item_dots_info[len(item_dots_info) - 1][1]))
-        print(
-            "my_dots_info[0][1] = " +
-            str(my_dots_info[0][1]))
-
-        distance = min(
-            math.fabs(
-                my_dots_info[len(my_dots_info) - 1][1] -
-                item_dots_info[0][1]),
-            math.fabs(
-                item_dots_info[len(item_dots_info) - 1][1] -
-                my_dots_info[0][1]))
-
+        distance = item_dots_info[-1] - my_dots_info[0]
         print("distance = " + str(distance))
 
         # 5. add new coordinate
         self.setPos((QVector2D(self.pos()) + move_dir * distance).toPointF())
-        print(
-            "new pos = " + str(
-                (QVector2D(self.pos()) + move_dir * distance).toPointF()))
 
-    def get_direction_info(self, item, direction):
-        # return list of tuples
-        # (vector to point, fabs of projection point to direction)
+    def get_direction_info(self, item, direction, center_global):
+        # return list of projection point to direction
+        # vector to point (from center_global point)
         # ----------------- x
         # |               |
         # |               |
@@ -161,43 +106,43 @@ class Personage(QGraphicsPixmapItem):
         # y
         item_vectors = []
         # top line
-        vector_to_point = QVector2D(
+        vector_to_point = QVector2D((
             QPointF(
                 item.mapToScene(
                     item.boundingRect().width() / 2,
-                    - item.boundingRect().height() / 2)))
-        item_vectors.append((
-            vector_to_point,
-            QVector2D.dotProduct(vector_to_point, direction)))
+                    - item.boundingRect().height() / 2))) -
+            center_global)
+        item_vectors.append(
+            QVector2D.dotProduct(vector_to_point, direction))
 
         # right line
         vector_to_point = QVector2D(
-            QPointF(
+            QPointF((
                 item.mapToScene(
                     - item.boundingRect().width() / 2,
-                    - item.boundingRect().height() / 2)))
-        item_vectors.append((
-            vector_to_point,
-            QVector2D.dotProduct(vector_to_point, direction)))
+                    - item.boundingRect().height() / 2))) -
+            center_global)
+        item_vectors.append(
+            QVector2D.dotProduct(vector_to_point, direction))
 
         # bottom line
         vector_to_point = QVector2D(
-            QPointF(
+            QPointF((
                 item.mapToScene(
                     - item.boundingRect().width() / 2,
-                    item.boundingRect().height() / 2)))
-        item_vectors.append((
-            vector_to_point,
-            QVector2D.dotProduct(vector_to_point, direction)))
+                    item.boundingRect().height() / 2))) -
+            center_global)
+        item_vectors.append(
+            QVector2D.dotProduct(vector_to_point, direction))
 
         # left line
         vector_to_point = QVector2D(
-            QPointF(
+            QPointF((
                 item.mapToScene(
                     item.boundingRect().width() / 2,
-                    item.boundingRect().height() / 2)))
-        item_vectors.append((
-            vector_to_point,
-            QVector2D.dotProduct(vector_to_point, direction)))
+                    item.boundingRect().height() / 2))) -
+            center_global)
+        item_vectors.append(
+            QVector2D.dotProduct(vector_to_point, direction))
 
         return item_vectors
